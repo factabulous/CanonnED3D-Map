230f2c14f67e13463e31cf25d301f92044708e65
Andy Mell <afmell@afmell.com>
2019-02-22 22:47:24 +0000

    Minor text changed

---
+++ Source/data/MapData-Lagrange.js
@@ -0,0 +1,17 @@
+var canonnEd3d_poi = {
+	init: function () {
+                $.getJSON("data/LagrangeClouds.json", function(info) { 
+                    Ed3d.init({
+                            container: 'edmap',
+                            json: info, 
+                            withFullscreenToggle: false,
+                            withHudPanel: true,
+                            hudMultipleSelect: true,
+                            effectScaleSystem: [50, 10000],
+                            startAnim: false,
+                            showGalaxyInfos: true,
+                            cameraPos: [25, 14100, -12900],
+                            systemColor: '#FF9D00'
+                    })});
+	}
+};
